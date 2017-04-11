674bbd604f27d4807aa1027f9b2b6228fd2d3467
moevegas <moevegas@gmail.com>
2017-04-11 17:17:41 -0400

    GiftExchange - basic

---
+++ GiftExchange/Models/Presents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GiftExchange.Models
+{
+    public class Presents
+    {
+        public Presents(SqlDataReader reader)
+        {
+            this.id = (int)reader["id"];
+            this.Contents = reader["Contents"].ToString();
+            this.GiftHint = reader["GiftHint"].ToString();
+            this.ColorWrapper = reader["ColorWrapper"].ToString();
+            this.Height = (int)reader["Height"];
+            this.Width = (int)reader["Width"];
+            this.Depth = (int)reader["Depth"];
+            this.Weight = (int)reader["Weight"];
+            this.IsOpened = (bool)reader["IsOpened"];
+        }
+
+        public int id { get; set; }
+        public string Contents { get; set; }
+        public string GiftHint { get; set; }
+        public string ColorWrapper { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Weight { get; set; }
+        public bool IsOpened { get; set; }
+    }
+}
---
+++ GiftExchange/Services/GiftService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GiftExchange.Services
+{
+    public class GiftService
+    {
+        // DatabaseLocation
+        const string connectionString =
+            @"Server=localhost\SQLEXPRESS;Database=GiftExchangeDB;Trusted_Connection=True;";
+        
+        public List<Models.Presents> GetGiftList ()
+        {
+            var rv = new List<Models.Presents>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = "SELECT * FROM PresentsTable";
+                var cmd = new SqlCommand(query, connection);
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    rv.Add(new Models.Presents(reader));
+                }
+                connection.Close();
+
+            }
+
+
+
+                return rv;
+        }
+
+    }
+}
