746fe37eeadf40feaa6e4a0544d673ea97878877
moevegas <moevegas@gmail.com>
2017-05-01 08:20:56 -0400

    Creating Gifts Works

--- GiftExchange/Services/GiftService.cs
+++ GiftExchange/Services/GiftService.cs
@@ -12,8 +12,8 @@ namespace GiftExchange.Services
         // DatabaseLocation
         const string connectionString =
             @"Server=localhost\SQLEXPRESS;Database=GiftExchangeDB;Trusted_Connection=True;";
-        
-        public List<Presents> GetGiftList ()
+
+        public List<Presents> GetGiftList()
         {
             var rv = new List<Presents>();
             using (var connection = new SqlConnection(connectionString))
@@ -31,17 +31,34 @@ namespace GiftExchange.Services
             return rv;
         }
 
-        public void AddGiftDB (Presents Present)
+        public Presents AddGiftDB(Presents Presents)
         {
             using (var connection = new SqlConnection(connectionString))
             {
-                var query = $"INSERT INTO [dbo].[PresentsFinal] ([Contents], [Gift Hint], [ColorWrapper], [Height], [Width], [Depth],[Weight],[IsOpened]) VALUES({Present.Contents}, {Present.GiftHint}, {Present.ColorWrapper}, {Present.Height}, {Present.Width}, {Present.Depth}, {Present.Weight}, {Present.IsOpened})";
+                var query = @"INSERT INTO [dbo].[PresentsFinal]
+                            ([Contents]
+                            ,[GiftHint]
+                            , [ColorWrapper]
+                            , [Height]
+                            , [Width]
+                            , [Depth]
+                            ,[Weight]
+                            ,[IsOpened])
+                            VALUES( @Contents, @GiftHint, @ColorWrapper, @Height, @Width, @Depth, @Weight, @IsOpened)";
                 var cmd = new SqlCommand(query, connection);
-                // cmd.Parameters.AddWithValue
+                cmd.Parameters.AddWithValue("@Contents", Presents.Contents);
+                cmd.Parameters.AddWithValue("@GiftHint", Presents.GiftHint);
+                cmd.Parameters.AddWithValue("@ColorWrapper", Presents.ColorWrapper);
+                cmd.Parameters.AddWithValue("@Height", Presents.Height);
+                cmd.Parameters.AddWithValue("@Width", Presents.Width);
+                cmd.Parameters.AddWithValue("@Depth", Presents.Depth);
+                cmd.Parameters.AddWithValue("@Weight", Presents.Weight);
+                cmd.Parameters.AddWithValue("@IsOpened", Presents.IsOpened);
                 connection.Open();
                 cmd.ExecuteNonQuery();
                 connection.Close();
             }
+            return Presents;
         }
     }
 }
